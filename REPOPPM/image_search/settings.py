d5dec92f779fb175d607bf9324edeba3b0d755b0
ciabatti <lorenzo.ciabatti@edu.unifi.it>
2025-02-09 19:50:20 +0100

    setting to deploy

--- REPOPPM/image_search/settings.py
+++ REPOPPM/image_search/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-_!^vl^%wh_f!@ndj3ndyl$4+_^wwn2xz2d*htpfemb2t33zmtj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
